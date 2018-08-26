787a36b80a8727db61c79e6b9066f8e626ea51b1
Damien Abeloos <dabeloos@w-r2-p2.s19.be>
2018-08-26 15:28:34 +0200

    ex06

--- ex06/do_op.c
+++ ex06/do_op.c
@@ -6,42 +6,42 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/24 16:36:31 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/26 14:45:18 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/26 15:28:16 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "do_op.h"
 
-void	ft_tab_init(int (*f)(int, int)[5])
+void	ft_tab_init(int (*f[5])(int, int))
 {
-	f[0] = &ft_add;
-	f[1] = &ft_sub;
-	f[2] = &ft_mul;
-	f[3] = &ft_div;
-	f[4] = &ft_mod;
+	*f[0] = &ft_add;
+	*f[1] = &ft_sub;
+	*f[2] = &ft_mul;
+	*f[3] = &ft_div;
+	*f[4] = &ft_mod;
 }
 
 int		main(int argc, char *argv[])
 {
-	int		(*f)(int, int)[5];
+	int		(*f[5])(int, int);
 
-	ft_tab_init(tab);
-	if (argc == 3)
+	ft_tab_init(&f);
+	if (argc == 4)
 	{
 		if (argv[2][0] == '+')
-			ft_putnbr(f[0](ft_atoi(argv[1]), ft_atoi(argv[2])));
+			ft_putnbr((f[0])(ft_atoi(argv[1]), ft_atoi(argv[3])));
 		else if (argv[2][0] == '-')
-			ft_putnbr(f[1](ft_atoi(argv[1]), ft_atoi(argv[2])));
+			ft_putnbr(f[1](ft_atoi(argv[1]), ft_atoi(argv[3])));
 		else if (argv[2][0] == '*')
-			ft_putnbr(f[2](ft_atoi(argv[1]), ft_atoi(argv[2])));
+			ft_putnbr(f[2](ft_atoi(argv[1]), ft_atoi(argv[3])));
 		else if (argv[2][0] == '/' && ft_atoi(argv[3]) == 0)
 			ft_putstr("Stop : division by zero");
 		else if (argv[2][0] == '%' && ft_atoi(argv[3]) == 0)
 			ft_putstr("Stop : modulo by zero");
 		else if (argv[2][0] == '/')
-			ft_putnbr(f[3](ft_atoi(argv[1]), ft_atoi(argv[2])));
+			ft_putnbr(f[3](ft_atoi(argv[1]), ft_atoi(argv[3])));
 		else if (argv[2][0] == '%')
-			ft_putnbr(f[4](ft_atoi(argv[1]), ft_atoi(argv[2])));
+			ft_putnbr(f[4](ft_atoi(argv[1]), ft_atoi(argv[3])));
 		else
 			ft_putnbr(0);
 		ft_putchar('\n');
