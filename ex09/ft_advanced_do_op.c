777b3a3401de8336631fd01501784c79f6d7703b
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-26 18:53:31 +0200

    ft_advanced_do_op

--- ex09/ft_advanced_do_op.c
+++ ex09/ft_advanced_do_op.c
@@ -6,11 +6,11 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/24 16:36:31 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/26 18:41:53 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/26 18:53:02 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_advanced_do_opp.h"
+#include "ft_advanced_do_op.h"
 #include "ft_opp.h"
 
 int		ft_usage(int a, int b)
@@ -20,7 +20,7 @@ int		ft_usage(int a, int b)
 	ft_putstr("error : only [ ");
 	while (++a < 5)
 	{
-		ft_putstr(g_opptab[a]);
+		ft_putstr(g_opptab[a].op);
 		ft_putchar(' ');
 	}
 	ft_putstr("] are accepted.\n");
@@ -38,18 +38,15 @@ int		main(int argc, char *argv[])
 			if (ft_strcmp(g_opptab[i].op, argv[2]) == 0)
 			{
 				if (ft_atoi(argv[3]) == 0 && (i == 3 || i == 4))
-				{
-					if (i == 3)
-						ft_putstr("Stop : division by zero\n");
-					else
+					(i == 3) ? ft_putstr("Stop : division by zero\n") :
 						ft_putstr("Stop : modulo by zero\n");
-				}
 				else
 				{
 					ft_putnbr(g_opptab[i].f(ft_atoi(argv[1]),
 								ft_atoi(argv[3])));
 					ft_putchar('\n');
 				}
+				break ;
 			}
 		return ((i == 5) ? g_opptab[i].f(0, 0) : 0);
 	}
